1e3a60df4f22f3d251587b016c574cf1b79bd353
DmitryG <dmitrygribchenko@mail.ru>
2022-11-28 02:10:45 +0300

    zad2

---
+++ seminar1/zad2.py
@@ -0,0 +1,7 @@
+OK = False
+for X in range(2):
+    for Y in range(2):
+        for Z in range(2):
+            if not(X or Y or Z) == (not (X) and not (Y) and not (Z)) :
+                OK=True
+            print(OK)
