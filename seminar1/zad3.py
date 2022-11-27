901ec20b3bf05dde4024c0a3d6ccb3e160788433
DmitryG <dmitrygribchenko@mail.ru>
2022-11-28 02:27:45 +0300

    zad3

---
+++ seminar1/zad3.py
@@ -0,0 +1,18 @@
+print("Enter X")
+x=int(input())
+print("Enter Y")
+y=int(input())
+if x==0:
+    print (Y)
+if y==0:
+    print (X)
+if x>0:
+    if y>0:
+        print("1")
+    else:
+        print("4")
+else:
+    if y>0:
+        print("2")
+    else:
+        print("3")
