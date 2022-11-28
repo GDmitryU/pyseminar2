b76478c698cb3e6f5b6d7e7bf8d8da12a8777771
DmitryG <dmitrygribchenko@mail.ru>
2022-11-29 00:22:36 +0300

    zad1(2)

---
+++ seminar2(2)/zad1(2).py
@@ -0,0 +1,11 @@
+rint("Enter number")
+number=float(input())
+stringOfNumber=str(number)
+stringOfNumber=stringOfNumber.replace(".","")
+number=int(stringOfNumber)
+summa=0
+while number>0:
+    mod=number%10
+    summa+=mod
+    number//=10
+print (summa)
